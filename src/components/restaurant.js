f37c597797c5bb6742098f574dd921c62aa550fd
nik-13 <nik-13@ya.ru>
2021-09-13 16:16:53 +0300

    add navigation

--- src/components/restaurant.js
+++ src/components/restaurant.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Menu from "./menu";
+import Navigation from "./navigation";
 
 
 
 export default function Restaurants (props){
+    const [activeId, setActiveId] = useState(props.restaurants[0].id)
+
+    const activeRestaurant = props.restaurants.find(restaurant => restaurant.id === activeId)
+
+
     return (
         <div>
-            <Menu menu = {props.restaurants[0].menu}/>
+            <Navigation
+                restaurants = {props.restaurants}
+                onRestaurantClick = {(id) => {setActiveId(id)} }
+            />
+            <Menu menu = {activeRestaurant.menu}/>
         </div>
     )
 }

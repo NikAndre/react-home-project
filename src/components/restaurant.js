cfc0e064b1566201ad42e2d264ceaf1172b3afc5
nik-13 <nik-13@ya.ru>
2021-09-14 08:59:57 +0300

    add navigation

--- src/components/restaurant.js
+++ src/components/restaurant.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{useState,useMemo} from 'react'
 import Menu from "./menu";
 import Navigation from "./navigation";
 
@@ -7,7 +7,10 @@ import Navigation from "./navigation";
 export default function Restaurants (props){
     const [activeId, setActiveId] = useState(props.restaurants[0].id)
 
-    const activeRestaurant = props.restaurants.find(restaurant => restaurant.id === activeId)
+    const activeRestaurant = useMemo(
+        () => props.restaurants.find(restaurant => restaurant.id === activeId),
+        [activeId,props.restaurants]
+    )
 
 
     return (

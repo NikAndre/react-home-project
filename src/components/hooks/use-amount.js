653edad39b3a80645437d7940d52685244241e6f
nik-13 <nik-13@ya.ru>
2021-09-14 10:03:21 +0300

    add navigation

---
+++ src/components/hooks/use-amount.js
@@ -0,0 +1,12 @@
+import {useState} from 'react'
+
+export default function useAmount(initialCount = 0){
+    const [count, setCount] = useState(initialCount)
+
+    const increment = () => setCount(count + 1)
+    const decrement = () => setCount(count < 0 ?  count - 1 : 0)
+
+
+
+    return {count,increment,decrement}
+}
--- src/components/product/product.js
+++ src/components/product/product.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import useAmount  from '../hooks/use-amount'
 
 
 export default function Product(props){
+   const {count,increment,decrement} = useAmount()
+
     return(
         <div>
             <p>{props.product.name}</p>
             <p>{props.product.price} $</p>
+            <button onClick={decrement}> - </button>
+            <p> count  : {count}</p>
+            <button onClick={increment}> + </button>
+
         </div>
     )
 
